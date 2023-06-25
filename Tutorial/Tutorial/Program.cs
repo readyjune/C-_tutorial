92aa251c085b2bcfd6a73498aefafa33c172fee6
Jinwoo Kim <jinwoo.kim@student.curtin.edu.au>
2023-06-25 20:06:13 +0800

    Add files via upload

---
+++ Tutorial/Tutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movie avengers = new Movie("The Avengers", "Joss WHedon", "Dog");
+
+            Console.WriteLine(avengers.Rating);
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
